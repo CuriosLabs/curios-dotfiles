ceb60e84e0368aa75418c692f7ce62f728c83b55
VideoCurio <david@videocurio.com>
2026-01-17 18:03:03 +0100

    Preparing next version

--- pkgs/curios-dotfiles/default.nix
+++ pkgs/curios-dotfiles/default.nix
@@ -4,13 +4,13 @@
 { lib, stdenvNoCC, fetchFromGitHub }:
 stdenvNoCC.mkDerivation rec {
   pname = "curios-dotfiles";
-  version = "0.11";
+  version = "0.12";
 
   src = fetchFromGitHub {
     owner = "CuriosLabs";
     repo = "curios-dotfiles";
     rev = version;
-    hash = "sha256-PjhfAvlruY29G7Inpn6TTOoOwp/ttz3ZDprsQCrWmNY=";
+    hash = "";
   };
 
   dontPatch = true;
