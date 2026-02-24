dba785d0eab842f3a1d2c20d4d6a7d7fd77c5bbe
VideoCurio <david@videocurio.com>
2026-02-24 13:59:49 +0100

    Updated nvim plugins

--- .config/nvim/lua/plugins/extra.lua
+++ .config/nvim/lua/plugins/extra.lua
@@ -5,6 +5,42 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  -- add any tools you want to have installed below
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+
   -- add DiffViewOpen
   { "sindrets/diffview.nvim" },
 }
