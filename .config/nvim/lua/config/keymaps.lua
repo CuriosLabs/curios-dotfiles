4849ed10068aed59c1bedeb609ad199aaf316da7
VideoCurio <david@videocurio.com>
2026-04-18 14:50:22 +0200

    Added: new neovim window mapping fo terminal and gemini

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,8 @@
 vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 -- Normal mode mappings
+vim.keymap.set("n", "<leader>wt", ":vsplit | terminal<CR>", { desc = "Terminal in vertical split" })
+vim.keymap.set("n", "<leader>wg", ":vsplit | terminal gemini<CR>", { desc = "Gemini in vertical split" })
 vim.keymap.set("n", "<leader><tab>t", ":tabnew | terminal<CR>", { desc = "Terminal in new tab" })
 vim.keymap.set("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
