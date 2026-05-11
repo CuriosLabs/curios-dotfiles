19f4f34df20bf4c56cdc9cb0ef504a7ff5d680f4
VideoCurio <david@videocurio.com>
2026-05-11 17:48:46 +0200

    Release 0.28.2

--- pkgs/curios-dotfiles/default.nix
+++ pkgs/curios-dotfiles/default.nix
@@ -4,13 +4,13 @@
 { lib, stdenvNoCC, fetchFromGitHub }:
 stdenvNoCC.mkDerivation rec {
   pname = "curios-dotfiles";
-  version = "0.28.1";
+  version = "0.28.2";
 
   src = fetchFromGitHub {
     owner = "CuriosLabs";
     repo = "curios-dotfiles";
     rev = version;
-    hash = "sha256-9B5ll7BsUMhyKoDdqIoxWlvZvt7vJ3u5l+AA8boT7zY=";
+    hash = "";
   };
 
   dontPatch = true;
