762320e0a0f233193d6da1eb20305c2157bb0665
VideoCurio <david@videocurio.com>
2026-09-02 20:31:36 +0200

    Updated hash signature

--- pkgs/curios-dotfiles/default.nix
+++ pkgs/curios-dotfiles/default.nix
@@ -10,7 +10,7 @@ stdenvNoCC.mkDerivation rec {
     owner = "CuriosLabs";
     repo = "curios-dotfiles";
     rev = version;
-    hash = "";
+    hash = "sha256-AzkHHkfFHTCHxiSO+y3VYk+mqIlG0SIlMBZ+H3O1JZ4=";
   };
 
   dontPatch = true;
